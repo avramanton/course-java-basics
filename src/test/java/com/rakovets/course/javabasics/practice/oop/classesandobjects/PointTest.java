f742604518e424711798733e47f57cf2deefeef0
Anton Avramenko <avramanton15730@mail.ru>
2020-10-15 01:11:13 +0300

    feat: add done taskTest 2 oop

---
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/PointTest.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point x = new Point(15, 20);
+        Point y = new Point(10, 25);
+        System.out.print(x);
+        System.out.print(y);
+    }
+}

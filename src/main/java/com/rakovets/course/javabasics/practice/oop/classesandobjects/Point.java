deea814335c46621357fdd3500f4ddfee36c895e
Anton Avramenko <avramanton15730@mail.ru>
2020-10-15 01:10:34 +0300

    feat: add done task 2 oop

---
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Point.java
@@ -0,0 +1,32 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+     public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int distance() {
+        return this.x + this.y;
+    }
+}
+
